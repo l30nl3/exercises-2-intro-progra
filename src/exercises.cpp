f08006a2c2fea011b6758904ca27542f74c7212c
l30nl3 <165826760+l30nl3@users.noreply.github.com>
2024-04-05 23:03:09 -0400

    1.5

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,30 +6,21 @@ using namespace std;
 
 void exercise_1(string s1) {
   // TODO: YOUR CODE HERE
-#include <iostream>
-#include <string>
-
-void dividirTexto(const std::string& linea) {
+void dividirTexto(const string& linea) {
     int longitud_palabra = 0;
 
     for (char caracter : linea) {
         if (caracter != ' ') {
             longitud_palabra++;
         } else {
-            std::cout << longitud_palabra << " ";
+            cout << longitud_palabra << " ";
             longitud_palabra = 0; // Reiniciar la longitud de la palabra para la siguiente palabra
         }
     }
 
-    std::cout << longitud_palabra << std::endl; // Imprimir la longitud de la última palabra
+    cout << longitud_palabra << endl; // Imprimir la longitud de la última palabra
 }
 
-int main() {
-    std::string linea;
-    std::getline(std::cin, linea); // Leer la línea desde la terminal
-    dividirTexto(linea);
-
-    return 0;
 }
 
 void exercise_2(string s1) {
