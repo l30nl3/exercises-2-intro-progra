55939d3be89b304a2ad2fe13365ea74a5fe20d8f
l30nl3 <165826760+l30nl3@users.noreply.github.com>
2024-04-06 01:51:10 -0400

    6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -113,30 +113,27 @@ void exercise_4(int n) {
     cout << factorial << endl;
 }
 //5
-  void imprimirCalendario(int primer_dia, int num_dias) {
-
-    for (int i = 1; i < primer_dia; ++i) {
-        std::cout << "   ";
-    }
-
-
-    for (int dia = 1; dia <= num_dias; ++dia) {
-
-        std::cout.width(2);
-        std::cout << dia;
-
-
-        if ((dia + primer_dia - 1) % 7 == 0 || dia == num_dias) {
-            std::cout << std::endl;
-        } else {
-
-            std::cout << ' ';
-        }
-    }
+void exercise_5(int n, int k) {
+  // TODO: YOUR CODE HERE
 }
 
 int exercise_6(int n) {
   // TODO: YOUR CODE HERE
+  #include <stdio.h>
+
+void sumarDigitos(int numero) {
+    int suma = 0;
+
+    // Iterar sobre cada dígito del número
+    while (numero > 0) {
+        suma += numero % 10; // Agregar el último dígito al resultado
+        numero /= 10; // Eliminar el último dígito del número
+    }
+
+    // Imprimir la suma
+    printf("%d\n", suma);
+}
+
   return 0;
 }
 
