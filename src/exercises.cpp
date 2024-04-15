e81d6c950e30e5cc66dc5653f879b0ba7dbb1d31
l30nl3 <165826760+l30nl3@users.noreply.github.com>
2024-04-14 22:24:03 -0400

    14

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -224,6 +224,22 @@ long exercise_13(int n, int k) {
     return sum;
 }
 
+string exercise_14(int n) {
+  // TODO: YOUR CODE HERE
+    int original = n;
+    int reversed = 0;
+    while (n > 0) {
+        int remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+    if (original == reversed) {
+        return "Es palindrome";
+    } else {
+        return "No es palindrome";
+    }
+}
+
 void exercise_15(int decimal) {
   // TODO: YOUR CODE HERE
 
